d174ab95c80cd8c437bbd0724e68d5387f1ae0e1
VettHor <horbovyivitalyi@gmail.com>
2022-12-16 09:45:42 +0200

    Created Models

---
+++ back-end/back-end/Models/Therapist.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.Build.Framework;
+using Newtonsoft.Json;
+
+namespace back_end.Models
+{
+    public class Therapist
+    {
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string ReceptionRoom { get; set; }
+
+        [Required]
+        public bool CanEdit { get; set; }
+    }
+}
---
+++ back-end/back-end/Program.cs
@@ -0,0 +1,37 @@
+using AutoMapper;
+using back_end.DB;
+//using back_end.Mapping;
+//using back_end.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+// Add services to the container.
+
+builder.Services.AddControllers();
+services.AddDbContext<DentistDbContext>(o => o.UseSqlServer(configuration["ConnectionString"]));
+//services.AddScoped<IPatientService, PatientsService>();
+//services.AddScoped<IMedicalHistoryService, MedicalHistoryService>();
+//services.AddSingleton<IMapper>(Mapping.Create());
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(policyBuilder => policyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
